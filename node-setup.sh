126f33224a7bc761e6e6086e5c17306a1bb46531
Ubuntu <ubuntu@ip-10-236-166-44.ec2.internal>
2013-07-19 10:24:34 +0000

    Temporary addition of a correct node.js setup script...

---
+++ node-setup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+sudo apt-get update
+sudo apt-get install -y python-software-properties python g++ make
+sudo add-apt-repository ppa:chris-lea/node.js
+sudo apt-get update
+sudo apt-get install -y nodejs
+npm --version
+node --version
